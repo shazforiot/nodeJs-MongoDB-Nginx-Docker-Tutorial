7316c974e1df3166dff4d214d04beab79e2dec0a
shazforiot <shazforiot@gmail.com>
2021-05-19 10:25:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const Item = require('./models/Item');
+
+app.get('/', (req, res) => {
+  Item.find()
+    .then(items => res.render('index', { items }))
+    .catch(err => res.status(404).json({ msg: 'No items found' }));
+});
+
+app.post('/item/add', (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  newItem.save().then(item => res.redirect('/'));
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log('Server running...'));
